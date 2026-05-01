093f83c57086450ecf61a3d655be40542fcaa3b0
Taush Sampley <taushsampley@gmail.com>
2026-04-30 19:28:30 -0500

    Add project page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import ProjectPage from "@/view/project-page";
+import { URL } from "url";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.ctas}>
+          { ProjectPage({ name: 'someproject', repo: URL.parse('https://github.com/Pointyware/static-site')!, description: 'The main static site source' }) }
         </div>
       </main>
     </div>
