127a6c899791bc7e3c979104acc346eae75da55e
Taush Sampley <taushsampley@gmail.com>
2026-05-11 21:31:48 -0500

    setup playwright for browser testing

---
+++ src/app/page.test.tsx
@@ -0,0 +1,8 @@
+import { test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Home from './page'
+
+test('', ()=>{
+  render(<Home />)
+  expect(screen.getByRole('heading', {level: 1, name: 'Title'}))
+})
