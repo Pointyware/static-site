6d6ba86ca1b6a0f105e82264c6cfc59aca91c259
Taush Sampley <taushsampley@gmail.com>
2026-05-12 12:23:52 -0500

    style project list

--- src/app/_components/project-list.tsx
+++ src/app/_components/project-list.tsx
@@ -1,15 +1,19 @@
 import ProjectView from "@/components/project-view"
 import { Project } from "@/model/project"
+import styles from '@/app/page.module.css'
 
 export interface List {
   projects: Project[]
 }
 
 export default function ProjectList({ projects }: List) {
-
-  return projects.map((project) => {
-    return <div key={project.name}>
-      <ProjectView project={project} />
-    </div>
-  })
+  return (
+    <div className={styles.projectList}>{
+      projects.map((project) => {
+        return (
+          <ProjectView  project={project} key={project.name} />
+        )
+      })
+    }</ div>
+  )
 }
