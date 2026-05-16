22b7631488dc010126f755689fd64cef5235dca9
Taush Sampley <taushsampley@gmail.com>
2026-05-16 17:00:34 -0500

    remove welcome from header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,10 +1,17 @@
-import Welcome from "./welcome";
 
 export default function Header() {
 
   return (
-    <header className='container'>
-      <Welcome />
+    <header>
+      <div>
+        <h1 aria-label='Title'>Pointyware</h1>
+        <p>Software with a Point</p>
+      </div>
+      <div>
+        {/* Account Icon */}
+        <h2>User</h2>
+        <p>User Account</p>
+      </div>
     </header>
   )
 }
