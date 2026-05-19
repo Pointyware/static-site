e356a0337433e3d3f909385bb24ef122755baee2
Taush Sampley <taushsampley@gmail.com>
2026-05-19 06:37:44 -0500

    Wrap page content in main

--- src/app/subjects/[...subjectName]/page.tsx
+++ src/app/subjects/[...subjectName]/page.tsx
@@ -1,19 +1,30 @@
+'use client'
 
-import { useRouter } from 'next/router'
+import { useParams } from 'next/navigation'
+import styles from '@/app/page.module.css'
 
 /**
  * 
  * @returns 
  */
 export default function SubjectPage() {
-  const router = useRouter()
-  const path = router.query.subjectName
+  const params = useParams<{ subjectName: string[] }>()
+  const path = params.subjectName
+
+  return (
+    <main className={styles.main}>
+      <SubjectView path={path}/>
+    </main>
+  )
+}
+
+export function SubjectView({path}:{path:any}) {
   if (path == undefined) {
     return <p>Undefined</p>
   }
   if (path instanceof Array) {
     return path.map((element)=>{
-      return <p>
+      return <p key={element}>
         { element }
       </p>
     })
