919108f8626fbf669b499b1ad8d4f951fa691b4f
Taush Sampley <taushsampley@gmail.com>
2026-06-04 15:43:53 -0500

    Move subject view and page to components

--- src/components/subject-view.tsx
+++ src/components/subject-view.tsx
@@ -3,20 +3,6 @@
 import { useParams } from 'next/navigation'
 import styles from '@/app/page.module.css'
 
-/**
- * 
- * @returns 
- */
-export default function SubjectPage() {
-  const params = useParams<{ subjectName: string[] }>()
-  const path = params.subjectName
-
-  return (
-    <main className={styles.main}>
-      <SubjectView path={path}/>
-    </main>
-  )
-}
 
 export function SubjectView({path}:{path:any}) {
   if (path == undefined) {
@@ -31,4 +17,19 @@ export function SubjectView({path}:{path:any}) {
   } else {
     return <p>{ path }</p>
   }
+}
+
+/**
+ * 
+ * @returns 
+ */
+export default function SubjectPage() {
+  const params = useParams<{ subjectName: string[] }>()
+  const path = params.subjectName
+
+  return (
+    <main className={styles.main}>
+      <SubjectView path={path}/>
+    </main>
+  )
 }
